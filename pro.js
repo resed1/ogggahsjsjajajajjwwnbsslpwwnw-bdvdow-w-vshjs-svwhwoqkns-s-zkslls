60daf39cde4fd0e0b3f6fca93fa982f23350ea0c
resed1 <mahmoudel88449@gmail.com>
2025-09-08 12:39:22 +0300

    pro.js

---
+++ pro.js
@@ -0,0 +1,9 @@
+const comingSoonPopup = document.getElementById('comingSoonPopup');
+
+function showComingSoonPopup() {
+    comingSoonPopup.classList.add('visible');
+}
+
+function hideComingSoonPopup() {
+    comingSoonPopup.classList.remove('visible');
+}
